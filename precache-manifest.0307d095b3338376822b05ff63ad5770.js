0d81256f01df01b3eb158ee490c452c4cf1d7425
Teequay <Teequay360@gmail.com>
2020-08-30 13:19:31 +0300

    Updates

---
+++ precache-manifest.0307d095b3338376822b05ff63ad5770.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f497b04c47b43c84e6d6",
+    "url": "/studyabroad/static/css/main.f497d201.chunk.css"
+  },
+  {
+    "revision": "f497b04c47b43c84e6d6",
+    "url": "/studyabroad/static/js/main.2ba016ff.chunk.js"
+  },
+  {
+    "revision": "dd664ef1a47980a157c7",
+    "url": "/studyabroad/static/js/runtime~main.e89f12a2.js"
+  },
+  {
+    "revision": "0e92877164d3cfa52d9f",
+    "url": "/studyabroad/static/js/2.af333514.chunk.js"
+  },
+  {
+    "revision": "b0177919009f3be9a044b3a299f8e025",
+    "url": "/studyabroad/static/media/studyabroadbanner08.b0177919.jpg"
+  },
+  {
+    "revision": "ea4a52cb02ad94d280bf480ecf0e817c",
+    "url": "/studyabroad/static/media/studyabroadbanner12.ea4a52cb.jpg"
+  },
+  {
+    "revision": "ce6df6b84ea006ab150d4646c80b6626",
+    "url": "/studyabroad/static/media/studyabroadbanner09.ce6df6b8.jpg"
+  },
+  {
+    "revision": "7ec0c9055ac79f23865ada32d15a1f26",
+    "url": "/studyabroad/index.html"
+  }
+];
